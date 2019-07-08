f8b3057e485450107b7b6f4c34e755493f26a0ea
pedia <ken.shao@gmail.com>
2019-07-08 21:22:22 +0800

    -

--- appone/echo/rpcuri_test.cc
+++ appone/echo/rpcuri_test.cc
@@ -4,6 +4,32 @@
 
 using namespace arch;
 
+TEST(Rpcuri, String) {
+  RpcUri result;
+  bool flag = false;
+
+  flag = RpcUri::Parse("http://localhost:80/foo.html?&q=1:2:3", &result);
+  EXPECT_TRUE(flag);
+
+  flag = RpcUri::Parse("https://localhost:80/foo.html?&q=1", &result);
+  EXPECT_TRUE(flag);
+
+  flag = RpcUri::Parse("localhost/foo", &result);
+  EXPECT_TRUE(flag);
+
+  flag = RpcUri::Parse("https://localhost/foo", &result);
+  EXPECT_TRUE(flag);
+
+  flag = RpcUri::Parse("localhost:8080", &result);
+  EXPECT_TRUE(flag);
+
+  flag = RpcUri::Parse("localhost?&foo=1", &result);
+  EXPECT_TRUE(flag);
+
+  flag = RpcUri::Parse("localhost?&foo=1:2:3", &result);
+  EXPECT_TRUE(flag);
+}
+
 TEST(Rpcuri, CreateChannelTest) {}
 
 // int main(int argc, char *argv[]) {
